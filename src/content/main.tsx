408224f70c0d3694062b877a6d26ca2c268ca2dc
Deri Kurniawan <deri.netuchi@gmail.com>
2025-08-23 03:33:03 +0700

    refactor: disable global CSS in content and update container id

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -1,11 +1,11 @@
-import "@/styles/global.css";
+// import "@/styles/global.css"; // tailwindcss affecting the current webview styles
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./views/App.tsx";
 
 const container = document.createElement("div");
-container.id = "crxjs-app";
+container.id = "sessify-webview";
 document.body.appendChild(container);
 createRoot(container).render(
   <StrictMode>
